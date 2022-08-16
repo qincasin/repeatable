976c137c5ee4c278b838a0057c0eaef3149644e3
秦加兴 <qincasin@163.com>
2022-08-16 22:14:34 +0800

    新增code smell module

---
+++ sample/code-smell/src/main/java/com/qjx/smell/CodeSmellApplication.java
@@ -0,0 +1,13 @@
+package com.qjx.smell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSmellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSmellApplication.class, args);
+    }
+
+}
---
+++ sample/code-smell/src/test/java/com/qjx/smell/CodeSmellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qjx.smell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSmellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
